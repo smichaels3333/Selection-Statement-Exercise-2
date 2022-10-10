34ed852adb1a0b5026f051136fbba919e4b6f059
Stephen Michaels <stephen.michaels3333@gmail.com>
2022-10-10 13:45:40 -0400

    initial commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); //Evaluate to a string
+
+            switch (subject)
+            {
+                case "Math";
+                    Console.WriteLine("Math is tough!");
+                    break;
+                case "Science";
+                    Console.WriteLine("Science is for smart people!");
+                    break;
+                case "English";
+                    Console.WriteLine("I can't read!");
+                    break;
+                case "History";
+                    Console.WriteLine("History is the best subject!");
+                    break;
+                default:
+                    Console.WriteLine($"Oh wow! I have not taken that subject before. {subject} sounds fun!");
+                    break;
+            }
         }
     }
 }
